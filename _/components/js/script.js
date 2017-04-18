8ef906367c3cddd41a423deffed356b1b98f00fb
codestruct <hello@helenshiu.com>
2017-04-18 00:40:35 -0400

    Add jquery and Change layout

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,13 +1,37 @@
-
+/*
 $("#ingredients-trigger").click(function(){
 	
 		$(".container").toggleClass("show").fadeIn(3000);
 	
 })
-
-/*
-(function() {
-  // Add event listener for clicks to ingredients and instructions
-  var 
-})();
 */
+$(function() {
+
+	var displayPanel = document.getElementById("display-content");
+	var ingredientContent = document.getElementById("ingredients-content").innerHTML;
+
+  $('.panel').click(function(event) {
+  	/*
+  	alert(displayPanel);
+  	*/
+  	  	displayPanel.innerHTML=ingredientContent;
+
+       if($("displayPanel").hasClass("show"))
+      {
+         // alert();
+       $("displayPanel").slideToggle("slow", function() { 
+         $("displayPanel").removeClass('show');
+      });
+        
+      } else {
+          
+          //alert('no class');
+      $("displayPanel").slideToggle("slow", function() { 
+         $("displayPanel").addClass('show'); 
+
+      });
+      }
+		
+  });
+  
+});
